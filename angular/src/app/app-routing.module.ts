a82bd49a37f46c8216734358c8b486a0251bdd5c
djm2x <dj-m2x@hotmail.com>
2020-11-11 15:58:23 +0100

    change name foler api to node

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'auth', pathMatch: 'full'},
   { path: 'file', loadChildren: () => import('./file/file.module').then(m => m.FileModule) },
-  { path: 'chat', loadChildren: () => import('./chat/chat.module').then(m => m.ChatModule) },
+  // { path: 'chat', loadChildren: () => import('./chat/chat.module').then(m => m.ChatModule) },
 ];
 
 @NgModule({
